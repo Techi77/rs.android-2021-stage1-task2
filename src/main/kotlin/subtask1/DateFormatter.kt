8197ac619c5766c83873c296afa65c52a971e342
Katharine Rossinskaya <rossinskaya98@gmail.com>
2021-06-19 20:11:53 +0300

    feat: add DateFormatter.kt

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,22 @@
 package subtask1
 
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
+
+
 class DateFormatter {
 
-    // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        //throw NotImplementedError("Not implemented")
+        val sdf1 = SimpleDateFormat("dd MM yyyy")
+        sdf1.isLenient = false
+        return try {
+            val c = sdf1.parse("$day $month $year")
+            val dayOfWeek = SimpleDateFormat("dd MMMM, EEEE", Locale("ru"))
+            (dayOfWeek.format(c))
+        } catch (e: Exception) {
+            ("Такого дня не существует")
+        }
     }
 }
