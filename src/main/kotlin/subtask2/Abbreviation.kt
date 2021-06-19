f58f844a232ff6fbd9ddd29986059bd155b9c3b0
Katharine Rossinskaya <rossinskaya98@gmail.com>
2021-06-19 21:06:47 +0300

    feat: add Abbreviation.kt

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -1,9 +1,20 @@
 package subtask2
 
+
 class Abbreviation {
 
-    // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        //throw NotImplementedError("Not implemented")
+        var aLow = a.toLowerCase()
+        val bLow = b.toLowerCase()
+            for(i in 0..bLow.lastIndex) {
+                if(aLow.contains(bLow[i])) {
+                    aLow = aLow.substringAfter(bLow[i])
+                }
+                else{
+                    return ("NO")
+                }
+            }
+            return ("YES")
     }
 }
